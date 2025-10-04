05bd034ed9e2814e3bb355e66f27a1a3478dff8c
ravitips <90592602+ravitips@users.noreply.github.com>
2025-10-04 12:35:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Hello from Flask in Docker on Azure VM! - Dockerized app"
+    return "This is a azure website - Created for Demo purpose"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
